0dbba01475e848cd2ed74cf2ed845172766e49b4
Kutuzov <artemij.kondr@yandex.ru>
2022-10-04 21:18:32 +1000

    Сделанное задание по Java от А

---
+++ Dz3/DogSC.java
@@ -0,0 +1,10 @@
+package Dz3;
+import java.util.ArrayList;
+public class DogSC {
+    public void scanDogs(ArrayList<DOG>dogs){
+        for (DOG dog:dogs)
+        {
+            System.out.println(dog.height);
+        }
+    }
+}
